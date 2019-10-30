9eecb7a391b73724fbb2d81deeca7f849d741662
Wyatt Weisensel <wweisensel@gmail.com>
2019-10-31 00:11:42 +0900

    data from jwt is loaded into state when refreshing the page or navigating to the site after logging in previously

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,8 +4,21 @@ import Main from './Main';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '../store';
+import { setAuthorizationToken, setCurrentUser } from '../store/actions/auth';
+import jwtDecode from 'jwt-decode';
 
 const store = configureStore();
+
+if (localStorage.jwtToken) {
+  setAuthorizationToken(localStorage.jwtToken);
+
+  // prevent someone from manually tampering with the key of jwtToken in localStorage
+  try {
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  } catch (e) {
+    store.dispatch(setCurrentUser({})); // forcefully log them out
+  }
+}
 
 function App() {
   return (
