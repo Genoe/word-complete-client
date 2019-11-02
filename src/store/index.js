c76df33853cb6efb6bfaaae95a16ac93d4720211
Wyatt Weisensel <wweisensel@gmail.com>
2019-11-02 16:27:07 +0900

    fixed issue where loading the redux devtools crashes the app if browser extentions aren't supported

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,7 @@ export function configureStore() {
         rootReducer,
         compose(
             applyMiddleware(thunk),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
         )
     );
 
