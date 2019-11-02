86ba1b804adfc74c9c129a515929936bb0ad5337
Wyatt Weisensel <wweisensel@gmail.com>
2019-11-02 16:18:21 +0900

    AccountForm styling is now closer to AuthForm styling

--- src/containers/AccountForm.js
+++ src/containers/AccountForm.js
@@ -38,20 +38,27 @@ class AccountForm extends Component {
     // making a inline function for onChange instead of a generic handleChange function since we only have one input (for the sake of conciseness)
     render() {
         return (
-            <form onSubmit={this.handleNewUsername}>
-                {this.props.errors.message && (
-                    <div className="alert alert-danger">{this.props.errors.message.join(', ')}</div>
-                )}
-                <input
-                    type="text"
-                    className="form-control"
-                    value={this.state.newUsername}
-                    onChange={e => this.setState({newUsername: e.target.value})}
-                />
-                <button type="submit" className="btn btn-success pull right">
-                    Update My Username!
-                </button>
-            </form>
+            <div className="row justify-content-md-center text-center">
+                <div className="col-md-6">
+                    <form onSubmit={this.handleNewUsername}>
+                        {this.props.errors.message && (
+                            <div className="alert alert-danger">{this.props.errors.message.join(', ')}</div>
+                        )}
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            id="username"
+                            name="username" 
+                            type="text"
+                            className="form-control"
+                            value={this.state.newUsername}
+                            onChange={e => this.setState({newUsername: e.target.value})}
+                        />
+                        <button type="submit" className="btn btn-primary btn-block btn-lg">
+                            Update My Username!
+                        </button>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
