332f5939d8ccc0443d2597ae09e3d1f35a4850b1
Wyatt Weisensel <wweisensel@gmail.com>
2019-12-07 21:43:36 +0900

    Took out link to unsplash image creator. Added links to github and linkedin

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,32 +5,24 @@ import Chatroom from '../containers/Chatroom';
 const Homepage = ({ currentUser }) => {
     if (!currentUser.isAuthenticated) {
         return (
-            <div className="home-hero">
-                <h1>What's Happening?</h1>
-                <h4>New to Word Complete?</h4>
-                <Link to="/signup" className="btn btn-primary">
-                    Sign Up Here
-                </Link>
-                {/* Credits For Unsplash image */}
-                <div className="unsplash-container">
-                    <a className="unsplash-credits"               
-                        href="https://unsplash.com/@brett_jordan?utm_medium=referral&utm_campaign=photographer-credit&utm_content=creditBadge"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title="Download free do whatever you want high-resolution photos from Brett Jordan"
-                        >
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                                <title>unsplash-logo</title>
-                                <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z" />
-                            </svg>
-                        </span>
-                        <span>
-                            Brett Jordan
-                        </span>
-                    </a>;
+            <div>
+                <div className="home-hero">
+                    <h1>What's Happening?</h1>
+                    <h4>New to Word Complete?</h4>
+                    <Link to="/signup" className="btn btn-primary">
+                        Sign Up Here
+                    </Link>
                 </div>
-            </div>
+
+                <div className="github-links text-center">
+                    <p>
+                        Created by <a href="https://www.linkedin.com/in/wyattw/" target="_blank" rel='noreferrer noopener'>Wyatt Weisensel</a>
+                        <br />
+                        Github: <a href="https://github.com/Genoe/word-complete-client" target="_blank" rel='noreferrer noopener'>Frontend </a>
+                        <a href="https://github.com/Genoe/word-complete-server" target="_blank" rel='noreferrer noopener'>Backend</a>
+                    </p>
+                </div>
+            </div>         
         );
     }
 
